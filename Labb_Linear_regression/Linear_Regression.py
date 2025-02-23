980f2aa7a24f51e0482a0f9f612e392c0857743f
S1gge <sigge.parakhnevich@gmail.com>
2025-02-23 13:56:35 +0300

    Fixing bugs

--- Labb_Linear_regression/Linear_Regression.py
+++ Labb_Linear_regression/Linear_Regression.py
@@ -9,37 +9,37 @@ class Linear_Regression:
         self.Y = Y
 
     @property
-    def fit (self):
+    def fit(self):
         return np.linalg.pinv(self.X.T @ self.X) @ self.X.T @ self.Y
     
     @property
-    def d (self):        # number of features of the model.
+    def d(self):        # number of features of the model.
         return self.X.shape[1]-1
     
     @property
-    def n (self):       # size of the sample.
+    def n(self):       # size of the sample.
         return self.Y.shape[0]
     
     @property
-    def SSE (self):     # SSE
+    def SSE(self):     # SSE
         return np.sum(np.square(self.Y-(self.X @ self.fit)))
         
     @property
-    def var (self):      # A function or method to calculate the variance.
+    def var(self):      # A function or method to calculate the variance.
         return self.SSE/(self.n-self.d-1)
     
     @property
-    def st_dev (self):      # A function or method to calculate the standard deviation.
+    def st_dev(self):      # A function or method to calculate the standard deviation.
         return np.sqrt(self.var)
     
     @property
-    def Syy (self):     # Calculate the total variability
+    def Syy(self):     # Calculate the total variability
         return (self.n*np.sum(np.square(self.Y)) - np.square(np.sum(self.Y)))/self.n
     
     @property
-    def SSR (self):     # Calculate the sum of squares due to the regression 
+    def SSR(self):     # Calculate the sum of squares due to the regression 
         return self.Syy - self.SSE
     
     @property
-    def Rsq (self):
+    def Rsq(self):
         return self.SSR/self.Syy
